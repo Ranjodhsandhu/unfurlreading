5fd85a133453e6d6ba48a0b4a2aa125b3af773da
Ranjodh Singh <ranjodhsingh24@gmail.com>
2021-08-22 17:27:54 -0400

    design for mobile devices

--- api/server.js
+++ api/server.js
@@ -40,13 +40,13 @@ const MONGO_CONFIG = {
   useCreateIndex: true,
 }
 
-// if(process.env.NODE_ENV === "production"){
+if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.resolve(__dirname,'../build')));
 
   app.get('*', (req,res) =>{
     res.sendFile(path.resolve(__dirname,'../build/index.html'));
   })
-// }
+}
 
 // 7. Start server
 mongoose
